203ff7e21ce1f6024d4e9a682f966c47cd82142e
Maria Loza <maria@thisdot.co>
2021-04-09 16:25:40 -0700

    refactor(): integrated react and typescript

---
+++ src/data/plots.ts
@@ -0,0 +1,12 @@
+export const plotsData = {
+  plotA: {
+    width: 5,
+    length: 5,
+    minimumWaterRequired: 1,
+  },
+  plotB: {
+    width: 5,
+    length: 2,
+    minimumWaterRequired: 1,
+  },
+};
